68ffde5616f51853299749e6989ae8bf0126256d
Jeff Chiu <jwchiu82@gmail.com>
2021-02-22 20:58:02 -0800

    add navbar to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,19 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 function Header(){
   return (
-    <h1>Help Queue</h1>
+    <>
+      <h1>Help Queue</h1>
+      <ul>
+        <li>
+          <Link to = "/">Home</Link>
+        </li>
+        <li>
+          <Link to="/signin">Sign In</Link>
+        </li>
+      </ul>
+    </>
   );
 }
 
